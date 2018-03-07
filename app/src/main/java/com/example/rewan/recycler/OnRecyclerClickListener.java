965e55fe0695ff9d0c01695b6780d5cd96ad0d8d
Bartosz Nowacki <bartosz.nowacki2@gmail.com>
2018-03-07 12:04:03 +0100

    Update OnRecyclerClickListener.java
    update

--- app/src/main/java/com/example/rewan/recycler/OnRecyclerClickListener.java
+++ app/src/main/java/com/example/rewan/recycler/OnRecyclerClickListener.java
@@ -3,7 +3,7 @@ package com.example.rewan.recycler;
 import android.view.View;
 
 /**
- * Listener interface for RecyclerView onClick event
+ * Listener interface for RecyclerView onClick event in DetailActivity. It is used by Videos Adapter
  */
 public interface OnRecyclerClickListener {
     void onItemClick(View view, int position);
