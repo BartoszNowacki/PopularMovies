1c892db1e48ca1cf780a01e771f91340e48d79fb
Bartosz Nowacki <bartosz.nowacki2@gmail.com>
2018-03-07 09:02:33 +0100

    Update BaseActivity.java
    Backdrop added

--- app/src/main/java/com/example/rewan/ui/BaseActivity.java
+++ app/src/main/java/com/example/rewan/ui/BaseActivity.java
@@ -12,6 +12,7 @@ public class BaseActivity extends AppCompatActivity {
             MovieContract.MovieEntry.COLUMN_TITLE,
             MovieContract.MovieEntry.COLUMN_RELEASE,
             MovieContract.MovieEntry.COLUMN_POSTER,
+            MovieContract.MovieEntry.COLUMN_BACKDROP,
             MovieContract.MovieEntry.COLUMN_VOTE,
             MovieContract.MovieEntry.COLUMN_PLOT,
             MovieContract.MovieEntry.COLUMN_MOVIE_ID,
@@ -20,7 +21,8 @@ public class BaseActivity extends AppCompatActivity {
     public static final int INDEX_MOVIE_TITLE = 1;
     public static final int INDEX_MOVIE_RELEASE = 2;
     public static final int INDEX_MOVIE_POSTER = 3;
-    public static final int INDEX_MOVIE_VOTE = 4;
-    public static final int INDEX_MOVIE_PLOT = 5;
-    public static final int INDEX_MOVIE_ID = 6;
+    public static final int INDEX_MOVIE_BACKDROP = 4;
+    public static final int INDEX_MOVIE_VOTE = 5;
+    public static final int INDEX_MOVIE_PLOT = 6;
+    public static final int INDEX_MOVIE_ID = 7;
 }
