8e639c1a03cd064db90166764c3d0cfa40f1e4e7
Bartosz Nowacki <bartosz.nowacki2@gmail.com>
2018-03-07 13:11:40 +0100

    Update ImagePathBuilder.java
    update

--- app/src/main/java/com/example/rewan/utils/ImagePathBuilder.java
+++ app/src/main/java/com/example/rewan/utils/ImagePathBuilder.java
@@ -3,11 +3,23 @@ package com.example.rewan.utils;
 
 import android.util.Log;
 
+/**
+     * Class for making correct url for images
+     */
+
 public class ImagePathBuilder {
+    
+    /**
+     * Method for create url for tmdb.org images
+     */
     public String posterPathBuilder(String endpoint){
         final String baseUrl =  "http://image.tmdb.org/t/p/w780";
         return baseUrl+endpoint;
     }
+    
+     /**
+     * Method for create url for youtube.com images
+     */
     public String youTubeImagePathBuilder(String id){
         final String baseUrl =  "https://img.youtube.com/vi/";
         final String endUrl = "/0.jpg";
