f3e0984f41952a4452daf1f5fdb72c19d0d6f700
Bartosz Nowacki <bartosz.nowacki2@gmail.com>
2018-03-07 09:00:03 +0100

    Update MovieContract.java
    Backdrop added

--- app/src/main/java/com/example/rewan/data/MovieContract.java
+++ app/src/main/java/com/example/rewan/data/MovieContract.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.rewan.data;
 
 import android.net.Uri;
@@ -40,7 +26,8 @@ public class MovieContract {
 
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_RELEASE = "release";
-        public static final String COLUMN_POSTER = "poster";
+        public static final String COLUMN_POSTER = "poster";     
+        public static final String COLUMN_BACKDROP = "backdrop";
         public static final String COLUMN_VOTE= "vote";
         public static final String COLUMN_PLOT= "plot";
         public static final String COLUMN_MOVIE_ID = "movie_id";
